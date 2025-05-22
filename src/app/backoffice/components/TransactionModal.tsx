a55d5af70d4773367b90a6b4abe5a7a8c43a7042
Lexie Wambui <wlexie123@gmail.com>
2025-05-22 17:41:31 +0300

    eslint rules

--- src/app/backoffice/components/TransactionModal.tsx
+++ src/app/backoffice/components/TransactionModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Transaction } from "../types/transactions";
 import dynamic from "next/dynamic";
