5208baf2f0687c77244941364ecc3a07533d8f9f
Lexie Wambui <wlexie123@gmail.com>
2025-05-26 23:11:25 +0300

    receipt

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@react-pdf/renderer'],
+  },
+
   images: {
     domains: ['flagcdn.com'],
-    // You can add other image domains if needed
+    // You can add other image domai needed
   },
   // Add the typescript block here
   typescript: {
