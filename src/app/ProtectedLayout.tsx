23717428ceceed9d6152c6ded7e4215f7e1eea03
Lexie Wambui <wlexie123@gmail.com>
2025-07-20 13:44:20 +0300

    timeout

--- src/app/ProtectedLayout.tsx
+++ src/app/ProtectedLayout.tsx
@@ -1,12 +1,11 @@
-"use client"; 
+"use client";
 
-import React from 'react';
-import { useInactivityLogout } from '../hooks/useInactivityLogout'; 
-// 3 hours in milliseconds
-const INACTIVITY_TIMEOUT = 20 * 60  * 1000;
+import React from "react";
+import { useInactivityLogout } from "../hooks/useInactivityLogout";
+// 30 minutes in milliseconds
+const INACTIVITY_TIMEOUT = 30 * 60 * 1000;
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
-
   useInactivityLogout(INACTIVITY_TIMEOUT);
 
   return <>{children}</>;
