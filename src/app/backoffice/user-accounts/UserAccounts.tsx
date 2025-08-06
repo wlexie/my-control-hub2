87c57ca28a208444964f96416cfa860e789422c0
Lexie Wambui <wlexie123@gmail.com>
2025-08-06 15:23:21 +0300

    search by onfido ID

--- src/app/backoffice/user-accounts/UserAccounts.tsx
+++ src/app/backoffice/user-accounts/UserAccounts.tsx
@@ -31,6 +31,7 @@ interface User {
     riskLevel?: string;
     totalScore?: number;
   };
+  onfidoApplicantId?: string;
 }
 
 export default function UserAccounts() {
@@ -144,6 +145,7 @@ export default function UserAccounts() {
         user.accountStatus?.toLowerCase() ?? "",
         user.accountId?.toString() ?? "",
         `tuma${user.accountId}`,
+        user.onfidoApplicantId?.toLowerCase() ?? "",
       ];
 
       // Date filter
