f93d52252e9a107edb3f1fca8ecf11e58582f0c5
jeremybundi <jeremybundi4@gmail.com>
2025-12-17 11:25:31 +0300

    removed dummy data plus eslint errors

--- src/utils/apiAuth.ts
+++ src/utils/apiAuth.ts
@@ -10,8 +10,8 @@ import type { Store } from "redux";
 import type { RootState } from "../store/store";
 
 // API base URL
-//const API_BASE_URL = "https://auth.tuma-app.com/api";
-const API_BASE_URL = "/api/auth";
+const API_BASE_URL = "https://auth.tuma-app.com/api";
+//const API_BASE_URL = "/api/auth";
 
 // Create Axios instance
 const api: AxiosInstance = axios.create({
