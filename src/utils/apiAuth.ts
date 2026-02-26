30462c8a2dd93a3d8c390aac62f2d780556fb482
Jeremiah Bundi Mong’are <jeremybundi4@gmail.com>
2026-02-26 16:33:47 +0300

    escalation history

--- src/utils/apiAuth.ts
+++ src/utils/apiAuth.ts
@@ -11,8 +11,8 @@ import type { RootState } from "../store/store";
 
 // API base URL
 
-const API_BASE_URL = "https://auth.tuma-app.com/api";
-//const API_BASE_URL = "/api/auth";
+//const API_BASE_URL = "https://auth.tuma-app.com/api";
+const API_BASE_URL = "/api/auth";
 
 // Create Axios instance
 const api: AxiosInstance = axios.create({
